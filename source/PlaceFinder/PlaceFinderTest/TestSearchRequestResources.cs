5d6f84a86eb5635a5f3470c6defd7d56eaa8dad6
Bjornep <bjorn.petersen.job@gmail.com>
2015-05-26 13:12:38 +0200

    Add commet out test as buildserver is building

--- source/PlaceFinder/PlaceFinderTest/TestSearchRequestResources.cs
+++ source/PlaceFinder/PlaceFinderTest/TestSearchRequestResources.cs
@@ -6,12 +6,43 @@ namespace GeodataStyrelsen.ArcMap.PlaceFinderTest
     [TestFixture]
     public class TestSearchRequestResources
     {
+        [Test]
+        public void TestGetResourceString_AddressAndPlaceName()
+        {
+            //Arrange
+            var searchRequestResources = new SearchRequestResources();
+            searchRequestResources.Addresses = true;
+            searchRequestResources.PlaceNames = true;
+            
+            //Act
+            string resourceString = searchRequestResources.GetResourceString;
 
+            //Assert
+            Assert.AreEqual("Adresser,Stednavne", resourceString);
+        }
 
         [Test]
-        public void TestFoobar()
+        public void TestGetResourceString_All()
         {
-            Assert.Fail("Remove this test");
+            //Arrange
+            var searchRequestResources = new SearchRequestResources();
+            searchRequestResources.Addresses = true;
+            searchRequestResources.CadastralNumber = true;
+            searchRequestResources.ElectoralDistrict = true;
+            searchRequestResources.HouseNumber = true;
+            searchRequestResources.JurisdictionsDistrict = true;
+            searchRequestResources.Municipalities = true;
+            searchRequestResources.PlaceNames = true;
+            searchRequestResources.PoliceDistrict = true;
+            searchRequestResources.PostDistricts = true;
+            searchRequestResources.Regions = true;
+            searchRequestResources.Street = true;
+
+            //Act
+            string resourceString = searchRequestResources.GetResourceString;
+
+            //Assert
+            Assert.AreEqual("Adresser,Veje,Husnumre,Kommuner,Matrikelnumre,Stednavne,Opstillingskredse,Politikredse,Postdistrikter,Regioner,Retskredse", resourceString);
         }
     }
 }
