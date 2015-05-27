be373077b8004b9ed2e854dc38773eec51d7e50e
Bjornep <bjorn.petersen.job@gmail.com>
2015-05-27 12:11:42 +0200

    Test of including a link til change log

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,16 @@
 console.log('This would be the main JS file.');
+
+function load() {
+    var file = new XMLHttpRequest();
+    file.open("GET", "https://raw.githubusercontent.com/geodatastyrelsen/stedfinder/master/changelog.md", true);
+    file.onreadystatechange = function() {
+      if (file.readyState === 4) {  // Makes sure the document is ready to parse
+        if (file.status === 200) {  // Makes sure it's found the file
+          text = file.responseText;
+          document.getElementById("change-log").innerHTML = text;
+        }
+      }
+    }
+}
+
+window.onLoad = load();
