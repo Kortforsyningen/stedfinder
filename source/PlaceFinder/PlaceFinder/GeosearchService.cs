2a5559c70647caaeb74365d76e38bc4d7f3fdba4
Bjornep <bjorn.petersen.job@gmail.com>
2015-06-03 15:30:58 +0200

    Added regx til search string

--- source/PlaceFinder/PlaceFinder/GeosearchService.cs
+++ source/PlaceFinder/PlaceFinder/GeosearchService.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.IO;
 using System.Net;
+using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
 using System.Text;
+using System.Text.RegularExpressions;
 using GeodataStyrelsen.ArcMap.PlaceFinder.Interface;
 
 namespace GeodataStyrelsen.ArcMap.PlaceFinder
@@ -13,22 +15,30 @@ namespace GeodataStyrelsen.ArcMap.PlaceFinder
         {
             try
             {
-                var searchText = Uri.EscapeDataString(searchRequestParams.SearchText.Replace('/', '_'));                
+                Regex pattern = new Regex("([\\\\#/])|(^[/&/./;])");
+                var searchText = searchRequestParams.SearchText; 
+                    //Uri.EscapeDataString(searchRequestParams.SearchText.Replace('/', '_').Replace('\\', '_').Replace('\0', '_').Replace('.', '_').Replace(';', '_'));
+                searchText = pattern.Replace(searchText, "_");
                 //TODO move resource to file
                 var url =
                     string.Format(
                         "https://kortforsyningen.kms.dk/Geosearch?type=json&search={0}&resources={1}&limit={2}&login={3}&password={4}&crs={5}",
-                        searchText, searchRequestParams.Resources, searchRequestParams.ReturnLimit, searchRequestParams.LoginName, searchRequestParams.Password,
+                        searchText, searchRequestParams.Resources, searchRequestParams.ReturnLimit,
+                        searchRequestParams.LoginName, searchRequestParams.Password,
                         searchRequestParams.EPSGCode);
 
-                var webClient = new WebClient { Encoding = Encoding.UTF8 };
+                var webClient = new WebClient {Encoding = Encoding.UTF8};
                 var jsonContent = webClient.DownloadString(url);
-                var serializer = new DataContractJsonSerializer(typeof(GeoSearchAddressData));
+                if (jsonContent.Equals("No workhorse available\r\nAll workhorses, including quarantined, are exhausted.\r\n"))
+                {
+                    throw new PlaceFinderException("Kan ikke søge på: '" + searchRequestParams.SearchText + "'");
+                }
+                var serializer = new DataContractJsonSerializer(typeof (GeoSearchAddressData));
 
                 GeoSearchAddressData response;
                 using (var ms = new MemoryStream(Encoding.Unicode.GetBytes(jsonContent)))
                 {
-                    response = (GeoSearchAddressData)serializer.ReadObject(ms);
+                    response = (GeoSearchAddressData) serializer.ReadObject(ms);
                 }
                 return response;
             }
--- source/PlaceFinder/PlaceFinderTest/TestGeosearchService.cs
+++ source/PlaceFinder/PlaceFinderTest/TestGeosearchService.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
+using System.Text.RegularExpressions;
+using GeodataStyrelsen.ArcMap.PlaceFinder;
+using GeodataStyrelsen.ArcMap.PlaceFinder.Interface;
 using NUnit.Framework;
 
 namespace GeodataStyrelsen.ArcMap.PlaceFinderTest
@@ -10,6 +9,24 @@ namespace GeodataStyrelsen.ArcMap.PlaceFinderTest
     [TestFixture]
     public class TestGeosearchService
     {
+        [Test]
+        public void TestRegX()
+        {
+            var pattern = new Regex("([\\\\#/])|(^[/&/./;])");
+            Assert.That(pattern.Replace("\\", "_"), Is.EqualTo("_"));
+            Assert.That(pattern.Replace("#", "_"), Is.EqualTo("_"));
+            Assert.That(pattern.Replace("/", "_"), Is.EqualTo("_"));
+            Assert.That(pattern.Replace("&", "_"), Is.EqualTo("_"));
+            Assert.That(pattern.Replace(".", "_"), Is.EqualTo("_"));
+            Assert.That(pattern.Replace(";", "_"), Is.EqualTo("_"));
+
+            Assert.That(pattern.Replace("A\\", "_"), Is.EqualTo("A_"));
+            Assert.That(pattern.Replace("A#", "_"), Is.EqualTo("A_"));
+            Assert.That(pattern.Replace("A/", "_"), Is.EqualTo("A_"));
+            Assert.That(pattern.Replace("A&", "_"), Is.EqualTo("A&"));
+            Assert.That(pattern.Replace("A.", "_"), Is.EqualTo("A."));
+            Assert.That(pattern.Replace("A;", "_"), Is.EqualTo("A;"));
+        }
         [Test]
         public void TestEncoding()
         {
@@ -19,6 +36,84 @@ namespace GeodataStyrelsen.ArcMap.PlaceFinderTest
             var escapeString = Uri.EscapeDataString(text);
             //Asset
             Assert.That(escapeString, Is.EqualTo("K%C3%B8benhavns%20Universitet%20%28Universitet%2FFagh%C3%B8jskole%20-%20K%C3%B8benhavn%29"));
+        }
+
+        [Test]
+        [Ignore("Integration test")]
+        public void TestSpecialCaratersSingel()
+        {
+            //Arange
+            var searchRequestParam = new SearchRequestParams();
+            var text = "D";
+            Console.Write(text);
+            searchRequestParam.SearchText = text;
+            var geosearchService = new GeosearchService();
+            //Act;
+            var geoSearchAddressData = geosearchService.Request(searchRequestParam);
+            //Asset
+            //Assert.That(geoSearchAddressData.message, Is.EqualTo("OK"), "Fail on text: " + text);
+            //Assert.That(geoSearchAddressData.status, Is.EqualTo("OK"), "Fail on text: " + text);
+            Assert.That(geoSearchAddressData.data.Count, Is.EqualTo(0), "Fail on text: " + text);
+        }
+
+        [Test]
+        [Ignore("Integration test")]
+        public void TestSpecialCaratersArrayWithAndAInfront()
+        {
+            //Arange
+            var searchRequestParam = new SearchRequestParams();
+            //for (int i = 32; i <= 165; i++)
+            for (int i = 1; i <= 255; i++)
+            {
+                var text = "A" + (char)i;
+                searchRequestParam.SearchText = text;
+                var geosearchService = new GeosearchService();
+                try
+                {
+                    //Act;
+                    var geoSearchAddressData = geosearchService.Request(searchRequestParam);
+                    //Asset
+                    var message = geoSearchAddressData.message;
+                    if (message != "OK")
+                    {
+                        Console.WriteLine(string.Format("\"{0}\"={1}", (char)i, message));
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(string.Format("\"{0}\"={1}", (char)i, e.Message));
+                }
+            }
+        }
+
+        [Test]
+        [Ignore("Integration test")]
+        public void TestSpecialCaratersArrayJustOne()
+        {
+            //Arange
+            var searchRequestParam = new SearchRequestParams();
+            //for (int i = 32; i <= 165; i++)
+            for (int i = 1; i <= 255; i++)
+            {
+                var text = ((char)i).ToString();
+                searchRequestParam.SearchText = text;
+                var geosearchService = new GeosearchService();
+                try
+                {
+                    //Act;
+                    var geoSearchAddressData = geosearchService.Request(searchRequestParam);
+                    //Asset
+                    var message = geoSearchAddressData.message;
+                    if (message != "OK")
+                    {
+                        Console.WriteLine(string.Format("\"{0}\"={1}", (char)i, message));
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(string.Format("\"{0}\"={1}", (char)i, e.Message));
+                }
+            }
         }
     }
 }
